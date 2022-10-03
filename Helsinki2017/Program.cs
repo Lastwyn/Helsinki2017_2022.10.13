8cc6f09d84f5259d35e46a5cca593db1f5154552
cisco <cisco@DESKTOP-D4D1G54>
2022-10-03 09:23:23 +0200

    Add project files.

---
+++ Helsinki2017/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Helsinki2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] soroknyers = File.ReadAllLines(@"donto.csv");
+            string[] sorok2nyers = File.ReadAllLines(@"rovidprogram.csv");
+            string[,] adatok = new string[5 , soroknyers.Length];
+            for (int i = 0; i < soroknyers.Length; i++)
+            {                 
+                    string[] s = soroknyers[i].Split(';');
+                    for (int k = 0; k < s.Length; k++)
+                    {
+                        adatok[k, i] = s[k];
+                        Console.WriteLine(adatok[k, i]);
+                    }                             
+            }
+            
+            Console.ReadKey();
+        }
+
+    }
+}
